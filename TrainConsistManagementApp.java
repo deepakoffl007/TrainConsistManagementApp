717f4ebb345120f5c58c53da1e65d440e566e6a5
DEEPAK <deepakelumalai.e@gmail.com>
2026-04-01 20:30:00 +0530

    UC3

--- TrainConsistManagementApp.java
+++ TrainConsistManagementApp.java
@@ -1,61 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        System.out.println("=================================");
-        System.out.println("UC2 - Add Passenger Bogies to Train");
-        System.out.println("=================================\n");
+        System.out.println("==================================");
+        System.out.println("UC3 - Track Unique Bogie IDs");
+        System.out.println("==================================\n");
 
-        String[] bogies = new String[10];
-        int count = 0;
+        Set<String> bogies = new HashSet<>();
 
-        bogies[count++] = "Sleeper";
-        bogies[count++] = "AC Chair";
-        bogies[count++] = "First Class";
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
 
-        System.out.print("After Adding Bogies:\nPassenger Bogies : [");
-        for (int i = 0; i < count; i++) {
-            System.out.print(bogies[i]);
-            if (i < count - 1) System.out.print(", ");
+        System.out.println("Unique Bogie IDs:");
+        for (String id : bogies) {
+            System.out.println(id);
         }
-        System.out.println("]");
-
-        int removeIndex = -1;
-        for (int i = 0; i < count; i++) {
-            if (bogies[i].equals("AC Chair")) {
-                removeIndex = i;
-                break;
-            }
-        }
-
-        if (removeIndex != -1) {
-            for (int i = removeIndex; i < count - 1; i++) {
-                bogies[i] = bogies[i + 1];
-            }
-            count--;
-        }
-
-        System.out.print("\nAfter Removing 'AC Chair':\nPassenger Bogies : [");
-        for (int i = 0; i < count; i++) {
-            System.out.print(bogies[i]);
-            if (i < count - 1) System.out.print(", ");
-        }
-        System.out.println("]");
-
-        boolean found = false;
-        for (int i = 0; i < count; i++) {
-            if (bogies[i].equals("Sleeper")) {
-                found = true;
-                break;
-            }
-        }
-
-        System.out.println("\nChecking if 'Sleeper' exists:");
-        System.out.println("Contains Sleeper? : " + found);
-
-        System.out.print("\nFinal Train Passenger Consist:\n[");
-        for (int i = 0; i < count; i++) {
-            System.out.print(bogies[i]);
-            if (i < count - 1) System.out.print(", ");
-        }
-        System.out.println("]");
     }
 }
